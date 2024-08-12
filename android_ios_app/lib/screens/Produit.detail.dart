405dde39fdbe55c8bc4f4fb053d498caf524b6e6
SamsonWilson <hugueswilson16@gmail.com>
2024-08-12 17:18:59 +0200

    suite

--- android_ios_app/lib/screens/Produit.detail.dart
+++ android_ios_app/lib/screens/Produit.detail.dart
@@ -1,73 +1,14 @@
-<<<<<<< HEAD
-import 'package:android_ios_app/commen/ui.dart';
-import 'package:android_ios_app/models/produit.model.dart';
 import 'package:flutter/material.dart';
 
 class ProduitDTScreen extends StatelessWidget {
-  static const routeName = 'produit-detail';
-=======
-import 'package:flutter/material.dart';
-
-class ProduitDTScreen extends StatelessWidget {
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
   const ProduitDTScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-
-    final produit = ModalRoute.of(context)!.settings.arguments as Produit;
-    return Scaffold(
-      appBar: AppBar(
-        title:  Text(produit.nom),
-      ),
-      body:Container(
-        decoration:DefaultScrennDecoretion,/** la decoration des produit */
-        child: ListView(
-        children: [
-           Hero(tag: produit.id, child: Image.network(produit.image),),
-          Padding(padding: const EdgeInsets.all(8.0),
-          child:
-
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.end,
-            children: [ 
-              Expanded(
-             
-                child: Text(produit.nom,
-                  style: const TextStyle(
-                    fontSize: 24,fontWeight: FontWeight.bold,
-                  ),
-              ),
-              ),
-              Text("\$${produit.prix}",
-              style: const TextStyle(
-                fontSize: 30,fontWeight: FontWeight.bold,
-                color: Colors.deepPurpleAccent,
-              ),
-              ),
-            ], 
-          ),
-           ),
-
-           Padding(padding: const EdgeInsets.all(8.0),
-            child: Text(produit.description,
-            style: const TextStyle(
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            ),
-            ),       
-        ],
-        ),
-      )
-=======
     return Scaffold(
       body: Center(
         child: Text("Les Details du Produit"),
       ),
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
     );
   }
 }
--- android_ios_app/lib/screens/produit.screen.dart
+++ android_ios_app/lib/screens/produit.screen.dart
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
-import 'package:android_ios_app/commen/ui.dart';
 import 'package:android_ios_app/models/produit.model.dart';
-import 'package:android_ios_app/screens/Produit.detail.dart';
-=======
-import 'package:android_ios_app/models/produit.model.dart';
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
 import 'package:android_ios_app/services/api.service.dart';
 import 'package:flutter/material.dart';
 
@@ -45,9 +39,6 @@ class _ProduitScreenState extends State<ProduitScreen> {
         ],
       ),
       body: Container(
-<<<<<<< HEAD
-      decoration: DefaultScrennDecoretion,/* la decoration des produit  */
-=======
         decoration: const BoxDecoration(
           gradient: LinearGradient(
               begin: Alignment.topLeft,
@@ -57,7 +48,6 @@ class _ProduitScreenState extends State<ProduitScreen> {
                 Color.fromARGB(255, 183, 66, 58),
               ]),
         ),
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
         child: FutureBuilder(
           future: futureProduit,
           builder: (context, snapshot) {
@@ -69,16 +59,8 @@ class _ProduitScreenState extends State<ProduitScreen> {
             if (snapshot.hasData) {
               return Center(
                 child: GridView.builder(
-<<<<<<< HEAD
-                  padding: const EdgeInsets.all(10.0),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      mainAxisSpacing: 10,
-                      crossAxisSpacing: 10,),
-=======
                   gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                       crossAxisCount: 2, childAspectRatio: 0.7),
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
                   itemCount: snapshot.data!.length,
                   itemBuilder: (context, index) {
                     return ProduitCart(produit: snapshot.data![index]);
@@ -100,111 +82,7 @@ class ProduitCart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-<<<<<<< HEAD
-    return InkWell(
-      onTap: (){
-        Navigator.pushNamed(context, ProduitDTScreen.routeName,arguments: produit);
-      },
-      child: ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child :Container(
-        decoration: const BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Stack(
-        children: [
-          Hero(tag: produit.id, child: Image.network(produit.image, width: 200, height: 100,),),
-           
-            /*Text(" Nom :${produit.nom}"),*/
-           Positioned(
-            bottom: 0,
-            left: 0,
-            child: Container(
-              color:Colors.white,
-              padding: const EdgeInsets.symmetric(
-                vertical: 7,
-                horizontal: 11,
-              ),
-              width: MediaQuery.of(context).size.width,
-              
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Text(
-              "${produit.prix}CFA",
-              style: TextStyle(fontSize: 10,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepPurple),
-            ),
-                Text(produit.nom.length > 15
-            ? " ${produit.nom.substring(0,15)}----"
-            :produit.nom,
-            style: const TextStyle(),),
-              ],
-            ),
-           ))
-        ],
-      ),
-      
-        
-      ),
-      
-    )
-
-    ) 
-    
-    
-    
-     /*ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child :Container(
-        decoration: const BoxDecoration(
-        color: Colors.white,
-      ),
-      child: Stack(
-        children: [
-           Image.network(produit.image, width: 200, height: 100,),
-            /*Text(" Nom :${produit.nom}"),*/
-           Positioned(
-            bottom: 0,
-            left: 0,
-            child: Container(
-              color:Colors.white,
-              padding: const EdgeInsets.symmetric(
-                vertical: 7,
-                horizontal: 11,
-              ),
-              width: MediaQuery.of(context).size.width,
-              
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-
-                Text(
-              "${produit.prix}CFA",
-              style: TextStyle(fontSize: 10,
-              fontWeight: FontWeight.bold,
-              color: Colors.deepPurple),
-            ),
-                Text(produit.nom.length > 15
-            ? " ${produit.nom.substring(0,15)}----"
-            :produit.nom,
-            style: const TextStyle(),),
-              ],
-            ),
-           ))
-        ],
-      ),
-      
-        
-      ),
-      
-    )*/;
-   /* Card(
-=======
     return Card(
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
@@ -212,20 +90,6 @@ class ProduitCart extends StatelessWidget {
             /*Text("${snapshot.data![index].nom}"),
                                    Image.network(
                                       "${snapshot.data![index].image}"),*/
-<<<<<<< HEAD
-            Image.network(produit.image, width: 100,),
-            /*Text(" Nom :${produit.nom}"),*/
-            Text("${produit.description}"),
-            Text(
-              "${produit.prix}CFA",
-              style: TextStyle(fontSize: 10),
-            ),
-            Text("${produit.stock}"),
-            Text("${produit.categorie}"),
-            Text(produit.nom.length > 15
-            ? " ${produit.nom.substring(0,15)}----"
-            :produit.nom),
-=======
             Image.network(produit.image),
             Text(" Nom :${produit.nom}"),
             Text("${produit.description}"),
@@ -235,15 +99,10 @@ class ProduitCart extends StatelessWidget {
             ),
             Text("${produit.stock}"),
             Text("${produit.categorie}"),
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
             
           ],
         ),
       ),
-<<<<<<< HEAD
-    );*/
-=======
     );
->>>>>>> 4cd1d19baf82a1b03fa244eb0d4b1e3913bdca20
   }
 }
